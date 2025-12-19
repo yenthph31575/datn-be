d4d1890436de72b621fb7d81dabc1f21f4575958
lqhuy2004 <huyquang1256@gmail.com>
2025-12-19 17:08:00 +0700

    update

--- src/shared/logger.ts
+++ src/shared/logger.ts
@@ -3,7 +3,7 @@ import * as util from 'util';
 import * as winston from 'winston';
 
 const ERROR_STASHES: string[] = [];
-let ERROR_SENDING_INTERVAL: numbers;
+let ERROR_SENDING_INTERVAL: number;
 if (process.env.ERROR_SENDING_INTERVAL) {
   ERROR_SENDING_INTERVAL = parseInt(process.env.ERROR_SENDING_INTERVAL, 10);
 }
