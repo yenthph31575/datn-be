d2885bddb6cbe815e716aaa5eb11e478a9845345
lqhuy2004 <huyquang1256@gmail.com>
2025-12-19 17:13:18 +0700

    update

--- src/shared/logger.ts
+++ src/shared/logger.ts
@@ -36,7 +36,7 @@ export function getLogger(name: string) {
       new winston.transports.Console({
         format: winston.format.combine(
           winston.format.colorize(),
-          winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:s' }),
+          winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
           winston.format.printf((info) => {
             const { timestamp, level, message, ...extra } = info;
             return `${timestamp} [${name}][${level}]: ${message} ${
