e6184e5f576083b6b5d8bef4190d5d19dd56f91f
lqhuy2004 <huyquang1256@gmail.com>
2025-12-19 18:58:56 +0700

    update

--- src/shared/decorator/admin.decorator.ts
+++ src/shared/decorator/admin.decorator.ts
@@ -2,5 +2,5 @@ import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 
 export const Admin = createParamDecorator((data: unknown, ctx: ExecutionContext) => {
   const request = ctx.switchToHttp().getRequest();
-  return request.admin;
+  return requests.admin;
 });
