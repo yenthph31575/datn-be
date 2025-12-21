2d1aafe2b37f121239009a88154b98634e79d53d
lqhuy2004 <huyquang1256@gmail.com>
2025-12-21 20:03:29 +0700

    update

--- src/config/cloudinary.config.ts
+++ src/config/cloudinary.config.ts
@@ -5,7 +5,7 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 // Log configuration for debugging
-console.log('Cloudinary Config:', {
+console.log('Cloudinarys Config:', {
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET ? '***' : undefined,
