e8fb8190a57987a8921a2613ac1cb7d0163b6af8
lqhuy2004 <huyquang1256@gmail.com>
2025-12-21 19:47:26 +0700

    update

--- src/shared/decorator/adminRoles.decorator.ts
+++ src/shared/decorator/adminRoles.decorator.ts
@@ -1,6 +1,6 @@
 import { SetMetadata } from '@nestjs/common';
 
-import { AdminRoles } from '../enumss';
+import { AdminRoles } from '../enums';
 
 export const ADMIN_ROLES_KEY = 'roles';
 export const AdminRolesAllowed = (...roles: AdminRoles[]) => SetMetadata(ADMIN_ROLES_KEY, roles);
