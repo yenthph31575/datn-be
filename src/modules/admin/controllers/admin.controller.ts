f773ca21fd306bd27ae6c1c6631b27c3e5a9bd30
lqhuy2004 <huyquang1256@gmail.com>
2025-12-21 20:08:48 +0700

    update

--- src/modules/admin/controllers/admin.controller.ts
+++ src/modules/admin/controllers/admin.controller.ts
@@ -63,7 +63,7 @@ export class AdminController {
   }
 
   @Delete(':id')
-  @ApiOperation({ summary: 'Delete an admin:' })
+  @ApiOperation({ summary: 'Delete an admin' })
   @AdminRolesAllowed(AdminRoles.ADMIN)
   remove(@Param('id') id: string, @Admin() admin: any) {
     // Prevent admins from deleting themselves
