9e4f1f3ca31ba2fc3f11e32b677fa147ea6a47a6
hoangtruong67 <hoangxuantruong2403@gmail.com>
2025-12-31 01:43:11 +0700

    Cập nhật cấu hình auth

--- src/config/auth.config.ts
+++ src/config/auth.config.ts
@@ -4,4 +4,5 @@ export default registerAs('auth', () => ({
   secret: process.env.JWT_SECRET_KEY,
   jwtExpires: process.env.JWT_EXPIRATION_TIME,
   refreshTokenTime: process.env.JWT_REFRESH_TOKEN_EXPIRATION_TIME,
+
 }));
