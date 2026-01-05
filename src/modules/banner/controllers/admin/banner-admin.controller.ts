0a657428af8a2e3b5b68e4a1690d314c9ca860a9
huylq2004 <huyquang1256@gmail.com>
2026-01-05 15:51:21 +0700

    update banner

--- src/modules/banner/controllers/admin/banner-admin.controller.ts
+++ src/modules/banner/controllers/admin/banner-admin.controller.ts
@@ -55,7 +55,6 @@ export class BannerAdminController {
     return this.bannerService.update(id, updateBannerDto);
   }
 
-  @Delete(':id')
   @ApiOperation({ summary: 'Delete banner' })
   @AdminRolesAllowed(AdminRoles.ADMIN)
   remove(@Param('id') id: string) {
